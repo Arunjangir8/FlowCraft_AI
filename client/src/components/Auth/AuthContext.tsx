38b674707c0b0746e386f1f2f74bc45cc1619944
Arunjangir8 <arunjangir9987@gmail.com>
2026-03-29 17:45:12 +0530

    refactor: clean up package.json and AuthContext.tsx Removed unused ESLint

--- client/src/components/Auth/AuthContext.tsx
+++ client/src/components/Auth/AuthContext.tsx
@@ -64,7 +64,6 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = (): AuthContextType => {
   return useContext(AuthContext);
 };
