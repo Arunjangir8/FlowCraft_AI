48c755216003a9352cac91fdd42f115772eb7ee4
Arunjangir8 <arunjangir9987@gmail.com>
2026-04-30 00:53:25 +0530

    refactor: remove unused import from PublicRoute component

--- client/src/components/Auth/PublicRoute.tsx
+++ client/src/components/Auth/PublicRoute.tsx
@@ -2,7 +2,6 @@ import { Navigate } from "react-router-dom";
 import type { JSX } from "react";
 import { useAuth } from "./AuthContext";
 import { DrawingLoader } from "../common/Loader";
-import { div } from "framer-motion/client";
 
 const PublicRoute = ({ children }: { children: JSX.Element }) => {
   const { user, loading } = useAuth();
